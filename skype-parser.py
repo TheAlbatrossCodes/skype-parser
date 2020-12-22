edf917045aeec15ddd4ccc44e7dee170ba2561b7
TheAlbatrossCodes <behnamsajadi@gmail.com>
2020-12-23 01:42:34 +0330

    Fix editted messages showing twice & better untar implementation

--- skype-parser.py
+++ skype-parser.py
@@ -1,19 +1,23 @@
-import json, re, argparse, tarfile
+import json
+import re
+import argparse
+import tarfile
+
 try:
     from bs4 import BeautifulSoup
-    beautifulsoup_imported = True
+    BEAUTIFULSOUP = True
 except ImportError:
-    beautifulsoup_imported = False
+    BEAUTIFULSOUP = False
     import html
 
 def main():
     args = get_commandline_args()
     # take the json file as input and open the skype json file
     main_file = read_file(args.filename) if not args.tar else read_tarfile(args.filename)
-   
-    if not beautifulsoup_imported:
-        print("\n{:-^50}\n".format('\nWARNING'))
-        print("BeautifulSoup is not installed onyour system. "
+
+    if not BEAUTIFULSOUP:
+        print("\n--- WARNING ---\n")
+        print("BeautifulSoup is not installed on your system. "
               "\nIt is safer to use this script with BeautifulSoup "
               "installed.\nYou can install BeautifuSoup using this "
               "command:\n\n\t\t pip install beautifulsoup4\n\n")
@@ -24,13 +28,13 @@ def main():
         user_display_name = input('\nPlease enter how you want your name to be displayed: ')
 
     # find the user's skype username & general metadata
-    user_id = main_file['userId'] 
+    user_id = main_file['userId']
     export_date, export_time = timestamp_parser(main_file['exportDate'])
     no_of_conversations = len(main_file['conversations'])
 
-    # store the usernames of everyone the user has chatted with and map to their prettier display name
+    # map username of everyone you chatted with to their display name
     ids = []
-    id_to_display_name = {user_id:str(user_display_name)}  # for now map your username to display name
+    id_to_display_name = {user_id:str(user_display_name)}
     export_file_name = {}  # the name we use for the chat log file
     messages_with_id = {}
 
@@ -64,15 +68,20 @@ def main():
             msg_from = messages_with_id[i][j]['from']
             msg_content = messages_with_id[i][j]['content']
             msg_type = messages_with_id[i][j]['messagetype']
-
-            # map the weirder message types to explanatory text 
+            # map the weirder message types to explanatory text
             if msg_type != 'RichText':
                 msg_content = type_parser(msg_type)
+
+            # edited messages show up twice, but so do normal messages sent twice
+            # so a safe way to get rid of them is as follows
+            if msg_content == messages_with_id[i][j-1]['content'] and re.search(r'<e_m.*>',  msg_content):
+                msg_content = ('--This user edited the following message at {}, you '
+                               'are viewing the edited version--'.format(msg_time))
 
             # construct how each individual message is going to look like
             message_content[msg_timestamp] = "[" + msg_time + "] " + id_to_display_name[msg_from] + ": " + msg_content
 
-        # now we have a dict whose key are the ids and it's content are messages displayed the way we want
+        # this dict will have ids as keys and actual messages as formatted vals
         message_box[i] = message_content
 
     # parse and clean the messages of it's weirder XML style tags and whatnot
@@ -86,17 +95,17 @@ def main():
             d, _ = timestamp_parser(j)
             if d not in date:
                 date.add(d)
-                compiled_message +="\n---------- Conversations on " + str(d) + " ----------\n"    
+                compiled_message +="\n---------- Conversations on " + str(d) + " ----------\n"
             compiled_message += message_box[person][j] + '\n'
 
-        if beautifulsoup_imported:
+        if BEAUTIFULSOUP:
             # get rid of the weirder skype XML
             pretty_parsed_content = content_parser(compiled_message)
             write_to_file(export_file_name[person], pretty_parsed_content)
 
         else:
-            # since no bs4, attempt to clear the XML using regex, but repeating messages are a problem
-            compiled_message = strip_tags(compiled_message)
+            # no bs4, so we use regex
+            compiled_message = tag_stripper(compiled_message)
             write_to_file(export_file_name[person], compiled_message)
 
     print("\nAll done!")
@@ -104,9 +113,14 @@ def main():
 
 def get_commandline_args():
     command = argparse.ArgumentParser("parser")
-    command.add_argument('filename', help='The path/name to the Skype json file you want to parse')
-    command.add_argument('-c', '--choose',action='store_true', help="Use this flag to choose which convos you'd like to parse")
-    command.add_argument('-t', '--tar', action='store_true', help='Use this flag to feed in a tar file, instead of a json file (at your own risk).')
+    command.add_argument('filename',
+                         help='The path/name to the Skype json file you want to parse')
+    command.add_argument('-c', '--choose',
+                         action='store_true',
+                         help="Use this flag to choose which convos you'd like to parse")
+    command.add_argument('-t', '--tar',
+                         action='store_true',
+                         help='Use this flag to feed in a tar file (at your own risk)')
     args = command.parse_args()
     return args
 
@@ -119,18 +133,25 @@ def read_file(filename):
 
 
 def read_tarfile(filename):
-    # needs a better implementation, would like to be able to search content of tar and just take .json out
+    # would rather a *.json pattern match implementation
     with tarfile.open(filename) as f:
+        # find files inside the tar
         tar_contents = f.getnames()
-        main_file = f.extractfile(f.getmember(tar_contents[0]))
+
+        # only get the file with .json extension
+        pattern = re.compile(r'.*\.json')
+        tar_files = list(filter(pattern.match, tar_contents))
+
+        # read that file and assign it to main_file
+        main_file = f.extractfile(f.getmember(tar_files[0]))
         main_file = json.load(main_file)
         return main_file
 
 
 def write_to_file(file_name, parsed_content):
-            with open(file_name, 'w+', encoding='utf-8') as f:
-                f.write(parsed_content)
-                print("Sucessfully parsed {}...".format(file_name))
+    with open(file_name, 'w+', encoding='utf-8') as f:
+        f.write(parsed_content)
+        print("Sucessfully parsed {}...".format(file_name))
 
 
 def type_parser(msg_type):
@@ -163,7 +184,7 @@ def content_parser(msg_content):
     return text
 
 
-def strip_tags(text):
+def tag_stripper(text):
     match = re.compile(r'<.*?>')
     text = match.sub('', text)
     text = html.unescape(text)
@@ -195,9 +216,10 @@ def id_selector(ids):
         print('\t{} ----> {}'.format(i+1, ids[i]))
         valid_selections[str(i+1)] = ids[i]
 
-    user_selection = input('Enter the number associated with the chats you want to export, separated by a space: ')
+    user_selection = input('Enter the number associated with the chats '
+                            'you want to export, separated by a space: ')
     selected_ids = set(user_selection.split())
-        
+
     while not selected_ids.issubset(set(valid_selections.keys())) or len(selected_ids) == 0:
         user_selection = input('Please enter valid numbers, separated by a space: ')
         selected_ids = set(user_selection.split())
